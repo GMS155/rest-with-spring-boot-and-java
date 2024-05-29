02a4c8d257e001e628cf05a5fdd6e0f6868574ff
GMS155 <gm.santos07@hotmail.com>
2024-05-29 14:56:12 -0300

    Criação de APIREST

---
+++ src/main/java/com/fab/treinamento/RestWithSpringBootAndJavaApplication.java
@@ -0,0 +1,16 @@
+package com.fab.treinamento;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootAndJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestWithSpringBootAndJavaApplication.class, args);
+
+    }
+
+
+}
